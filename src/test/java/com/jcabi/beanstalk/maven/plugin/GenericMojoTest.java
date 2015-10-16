e142fb47f3e3939e2dd0d637c69c1fa4239046c8
Piotr Pradzynski <prondzyn@gmail.com>
2015-10-16 10:59:25 +0200

    code improvements after code review

--- src/test/java/com/jcabi/beanstalk/maven/plugin/GenericMojoTest.java
+++ src/test/java/com/jcabi/beanstalk/maven/plugin/GenericMojoTest.java
@@ -85,14 +85,13 @@ public final class GenericMojoTest {
      */
     @Test
     public void validatesBadJson() throws Exception {
-        final DeployMojo mojo = new DeployMojo();
         Assert.assertFalse(
-            mojo.validJson(new StringBuilder()
-                    .append("[\n")
-                    .append("id: 102\n")
-                    .append("name: \"Rudy\"\n")
-                    .append("colors: [green, yellow]\n")
-                    .append("]\n").toString()
+            new DeployMojo().validJson(new StringBuilder()
+                .append("[\n")
+                .append("id: 102\n")
+                .append("name: \"Rudy\"\n")
+                .append("colors: [green, yellow]\n")
+                .append("]\n").toString()
             )
         );
     }
@@ -103,14 +102,13 @@ public final class GenericMojoTest {
      */
     @Test
     public void validatesGoodJson() throws Exception {
-        final DeployMojo mojo = new DeployMojo();
         Assert.assertTrue(
-            mojo.validJson(new StringBuilder()
-                    .append("{\n")
-                    .append("\"id'\": 102,\n")
-                    .append("\"name\": \"Rudy\",\n")
-                    .append("\"colors\": [\"green\", \"yellow\"],\n")
-                    .append("}\n").toString()
+            new DeployMojo().validJson(new StringBuilder()
+                .append("{\n")
+                .append("\"id'\": 102,\n")
+                .append("\"name\": \"Rudy\",\n")
+                .append("\"colors\": [\"green\", \"yellow\"],\n")
+                .append("}\n").toString()
             )
         );
     }
@@ -121,15 +119,14 @@ public final class GenericMojoTest {
      */
     @Test
     public void validatesBadYaml() throws Exception {
-        final DeployMojo mojo = new DeployMojo();
         Assert.assertFalse(
-            mojo.validYaml(new StringBuilder()
-                    .append("Some illegal Prefix\n")
-                    .append("Time: 2005-11-23 10:01:42 -5\n")
-                    .append("Admin: ed\n")
-                    .append("Messages:\n")
-                    .append(" Hello is an error information\n")
-                    .append(" for the configuration file\n").toString()
+            new DeployMojo().validYaml(new StringBuilder()
+                .append("Some illegal Prefix\n")
+                .append("Time: 2005-11-23 10:01:42 -5\n")
+                .append("Admin: ed\n")
+                .append("Messages:\n")
+                .append(" Hello is an error information\n")
+                .append(" for the configuration file\n").toString()
             )
         );
     }
@@ -140,15 +137,13 @@ public final class GenericMojoTest {
      */
     @Test
     public void validatesGoodYaml() throws Exception {
-        final DeployMojo mojo = new DeployMojo();
         Assert.assertTrue(
-            mojo.validYaml(
-                new StringBuilder()
-                    .append("Time: 2001-11-23 15:01:42 -5\n")
-                    .append("User: ed\n")
-                    .append("Warning:\n")
-                    .append("  This is an error message\n")
-                    .append("  for the log file\n").toString()
+            new DeployMojo().validYaml(new StringBuilder()
+                .append("Time: 2001-11-23 15:01:42 -5\n")
+                .append("User: ed\n")
+                .append("Warning:\n")
+                .append("  This is an error message\n")
+                .append("  for the log file\n").toString()
             )
         );
     }
